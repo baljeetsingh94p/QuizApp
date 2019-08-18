42a159b422ba8ec33b1f6e5fef98b5c72c70ba6e
baljeet singh <39096183+baljeetsingh94p@users.noreply.github.com>
2019-08-18 03:15:34 -0400

    working
    both are working:Login and Registration

--- LoginActivity.java
+++ LoginActivity.java
@@ -12,7 +12,6 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
-
 import com.muskan.quizapp.api.ApiConnection;
 import com.muskan.quizapp.api.OnApiResponseListener;
 
